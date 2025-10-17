c8caf729ba21c816202115a851bc789baa89b6e3
kenny <kennysaenz31@gmail.com>
2025-10-17 09:12:55 -0600

    Culminacion de todos los procesos front y bakc (Crud completos)

--- backend/src/Domain/Entities/Cuenta.cs
+++ backend/src/Domain/Entities/Cuenta.cs
@@ -3,11 +3,11 @@ public enum TipoCuenta { Ahorros = 1, Corriente = 2 }
 
 public class Cuenta
 {
-    public int Id { get; set; } // PK
+    public int Id { get; set; }
     public string Numero { get; set; } = null!;
     public TipoCuenta Tipo { get; set; }
     public decimal SaldoInicial { get; set; }
-    public bool Activa { get; set; }               // estado
+    public bool Activa { get; set; }
     public int ClienteIdFk { get; set; }
     public Cliente Cliente { get; set; } = null!;
     public ICollection<Movimiento> Movimientos { get; set; } = new List<Movimiento>();
